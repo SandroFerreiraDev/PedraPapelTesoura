8b870f40c869e85c5a32a5721f74f74c687cf083
SandroFerreiraDev <sandro.ferreira.developer@gmail.com>
2024-05-06 10:56:16 -0300

    feat: Added 1x1 and 1x1x1 activities and layouts

---
+++ app/src/main/java/com/ferreirasandro/pedrapapeltesoura/Game1x1x1Activity.kt
@@ -0,0 +1,11 @@
+package com.ferreirasandro.pedrapapeltesoura
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Game1x1x1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_1x1x1)
+    }
+}
--- app/src/main/java/com/ferreirasandro/pedrapapeltesoura/MainActivity.kt
+++ app/src/main/java/com/ferreirasandro/pedrapapeltesoura/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.ferreirasandro.pedrapapeltesoura
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,17 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btn1x1 = findViewById<Button>(R.id.BT1x1)
+        btn1x1.setOnClickListener {
+            val intent = Intent(this, Game1x1Activity::class.java)
+            startActivity(intent)
+        }
+        val btn1x1x1 = findViewById<Button>(R.id.BT1x1x1)
+        btn1x1x1.setOnClickListener {
+            val intent = Intent(this, Game1x1x1Activity::class.java)
+            startActivity(intent)
         }
     }
 }
