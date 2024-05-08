1a72f4ac692ae0d0bb2ec86afecf819d5b5bba96
Sandro <Sandro.ferreira.developer@gmail.com>
2024-05-08 08:47:05 -0300

    feat:added spock and lizard hand types

--- app/src/main/java/com/ferreirasandro/pedrapapeltesoura/Game1x1Activity.kt
+++ app/src/main/java/com/ferreirasandro/pedrapapeltesoura/Game1x1Activity.kt
@@ -21,13 +21,15 @@ class Game1x1Activity : AppCompatActivity() {
         imgPlayerChoice = findViewById(R.id.imgPlayerChoice)
         imgComputerChoice = findViewById(R.id.imgComputerChoice)
 
+        findViewById<Button>(R.id.btnSpock).setOnClickListener { playGame(CHOICE_SPOCK) }
+        findViewById<Button>(R.id.btnLizard).setOnClickListener { playGame(CHOICE_LIZARD) }
         findViewById<Button>(R.id.btnRock).setOnClickListener { playGame(CHOICE_ROCK) }
         findViewById<Button>(R.id.btnPaper).setOnClickListener { playGame(CHOICE_PAPER) }
         findViewById<Button>(R.id.btnScissors).setOnClickListener { playGame(CHOICE_SCISSORS) }
     }
 
     private fun playGame(playerChoice: Int) {
-        val computerChoice = Random.nextInt(3)
+        val computerChoice = Random.nextInt(5)
 
         imgPlayerChoice.setImageResource(getImageResource(playerChoice))
         imgComputerChoice.setImageResource(getImageResource(computerChoice))
@@ -42,6 +44,9 @@ class Game1x1Activity : AppCompatActivity() {
             CHOICE_ROCK -> R.drawable.ic_rock
             CHOICE_PAPER -> R.drawable.ic_paper
             CHOICE_SCISSORS -> R.drawable.ic_scissors
+            CHOICE_SPOCK -> R.drawable.ic_spock
+            CHOICE_LIZARD-> R.drawable.ic_lizard
+
             else -> throw IllegalArgumentException("Escolha inválida: $choice")
         }
     }
@@ -51,7 +56,14 @@ class Game1x1Activity : AppCompatActivity() {
             playerChoice == computerChoice -> "Empate"
             playerChoice == CHOICE_ROCK && computerChoice == CHOICE_SCISSORS ||
                     playerChoice == CHOICE_PAPER && computerChoice == CHOICE_ROCK ||
-                    playerChoice == CHOICE_SCISSORS && computerChoice == CHOICE_PAPER -> "Você ganhou!"
+                    playerChoice == CHOICE_SCISSORS && computerChoice == CHOICE_PAPER ||
+                    playerChoice == CHOICE_SCISSORS && computerChoice == CHOICE_LIZARD ||
+                    playerChoice == CHOICE_PAPER && computerChoice == CHOICE_SPOCK ||
+                    playerChoice == CHOICE_SPOCK && computerChoice == CHOICE_ROCK ||
+                    playerChoice == CHOICE_SPOCK && computerChoice == CHOICE_SCISSORS ||
+                    playerChoice == CHOICE_LIZARD && computerChoice == CHOICE_SPOCK ||
+                    playerChoice == CHOICE_LIZARD && computerChoice == CHOICE_PAPER ||
+                    playerChoice == CHOICE_ROCK && computerChoice == CHOICE_LIZARD-> "Você ganhou!"
             else -> "Você perdeu!"
         }
     }
@@ -60,6 +72,8 @@ class Game1x1Activity : AppCompatActivity() {
         private const val CHOICE_ROCK = 0
         private const val CHOICE_PAPER = 1
         private const val CHOICE_SCISSORS = 2
+        private const val CHOICE_SPOCK = 3
+        private const val CHOICE_LIZARD = 4
     }
 }
 
--- app/src/main/java/com/ferreirasandro/pedrapapeltesoura/Game1x1x1Activity.kt
+++ app/src/main/java/com/ferreirasandro/pedrapapeltesoura/Game1x1x1Activity.kt
@@ -23,14 +23,16 @@ class Game1x1x1Activity : AppCompatActivity() {
         imgComputerChoice1 = findViewById(R.id.imgComputerChoice1)
         imgComputerChoice2 = findViewById(R.id.imgComputerChoice2)
 
+        findViewById<Button>(R.id.btnSpock).setOnClickListener { playGame(CHOICE_SPOCK) }
+        findViewById<Button>(R.id.btnLizard).setOnClickListener { playGame(CHOICE_LIZARD) }
         findViewById<Button>(R.id.btnRock).setOnClickListener { playGame(CHOICE_ROCK) }
         findViewById<Button>(R.id.btnPaper).setOnClickListener { playGame(CHOICE_PAPER) }
         findViewById<Button>(R.id.btnScissors).setOnClickListener { playGame(CHOICE_SCISSORS) }
     }
 
     private fun playGame(playerChoice: Int) {
-        val computerChoice1 = Random.nextInt(3)
-        val computerchoice2 = Random.nextInt(3)
+        val computerChoice1 = Random.nextInt(5)
+        val computerchoice2 = Random.nextInt(5)
 
         imgPlayerChoice.setImageResource(getImageResource(playerChoice))
         imgComputerChoice1.setImageResource(getImageResource(computerChoice1))
@@ -46,6 +48,9 @@ class Game1x1x1Activity : AppCompatActivity() {
             CHOICE_ROCK -> R.drawable.ic_rock
             CHOICE_PAPER -> R.drawable.ic_paper
             CHOICE_SCISSORS -> R.drawable.ic_scissors
+            CHOICE_SPOCK -> R.drawable.ic_spock
+            CHOICE_LIZARD -> R.drawable.ic_lizard
+
             else -> throw IllegalArgumentException("Escolha inválida: $choice")
         }
     }
@@ -53,8 +58,27 @@ class Game1x1x1Activity : AppCompatActivity() {
     private fun getResult(playerChoice: Int, computerChoice1: Int, computerChoice2: Int): String {
         return when {
             playerChoice == computerChoice1 && playerChoice == computerChoice2 -> "Empate"
-            playerChoice == computerChoice1 || playerChoice == computerChoice2 ||
-                    computerChoice1 == computerChoice2 -> "Você ganhou!"
+
+
+            (playerChoice == CHOICE_PAPER && computerChoice1 == CHOICE_ROCK ||
+                    playerChoice == CHOICE_SCISSORS && computerChoice1 == CHOICE_PAPER ||
+                    playerChoice == CHOICE_SCISSORS && computerChoice1 == CHOICE_LIZARD ||
+                    playerChoice == CHOICE_PAPER && computerChoice1 == CHOICE_SPOCK ||
+                    playerChoice == CHOICE_SPOCK && computerChoice1 == CHOICE_ROCK ||
+                    playerChoice == CHOICE_SPOCK && computerChoice1 == CHOICE_SCISSORS ||
+                    playerChoice == CHOICE_LIZARD && computerChoice1 == CHOICE_SPOCK ||
+                    playerChoice == CHOICE_LIZARD && computerChoice1 == CHOICE_PAPER ||
+                    playerChoice == CHOICE_ROCK && computerChoice1 == CHOICE_LIZARD) &&
+                    (playerChoice == CHOICE_PAPER && computerChoice2 == CHOICE_ROCK ||
+                    playerChoice == CHOICE_SCISSORS && computerChoice2 == CHOICE_PAPER ||
+                    playerChoice == CHOICE_SCISSORS && computerChoice2 == CHOICE_LIZARD ||
+                    playerChoice == CHOICE_PAPER && computerChoice2 == CHOICE_SPOCK ||
+                    playerChoice == CHOICE_SPOCK && computerChoice2 == CHOICE_ROCK ||
+                    playerChoice == CHOICE_SPOCK && computerChoice2 == CHOICE_SCISSORS ||
+                    playerChoice == CHOICE_LIZARD && computerChoice2 == CHOICE_SPOCK ||
+                    playerChoice == CHOICE_LIZARD && computerChoice2 == CHOICE_PAPER ||
+                    playerChoice == CHOICE_ROCK && computerChoice2 == CHOICE_LIZARD)
+            -> "Você ganhou!"
             else -> "Você perdeu!"
         }
     }
@@ -63,5 +87,7 @@ class Game1x1x1Activity : AppCompatActivity() {
         private const val CHOICE_ROCK = 0
         private const val CHOICE_PAPER = 1
         private const val CHOICE_SCISSORS = 2
+        private const val CHOICE_SPOCK = 3
+        private const val CHOICE_LIZARD = 4
     }
 }
